bcd94ae7f633ba10a4d5e10897037491be0003de
AwesomeGearBoy <schroederat26@gmail.com>
2024-10-18 17:58:41 -0400

    gyhjfghj

--- PasswordControl.java
+++ PasswordControl.java
@@ -3,8 +3,8 @@ import java.util.Random;
 public class PasswordControl {
     public String randomize() {
         Random rand = new Random();
-        String[] passwords = new String[] { "password1", "password2", "password3" }; // Enter passwords like shown, making sure not to mess with code.
-        int randomPassword = rand.nextInt(0, 3); // Make sure second number here is the number of passwords you have available, otherwise some may not show up.
+        String[] passwords = new String[] { "Hocu$Pocu$10", "password2", "password3", "password4", "password5" }; // Enter passwords like shown, making sure not to mess with code.
+        int randomPassword = rand.nextInt(0, 5); // Make sure second number here is the number of passwords you have available, otherwise some may not show up.
         return passwords[randomPassword];
     }
 }
