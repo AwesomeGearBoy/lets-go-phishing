f3633b25ad1b617f96df3a90a14f6024a195e7a4
AwesomeGearBoy <schroederat26@gmail.com>
2024-10-20 17:48:45 -0400

    updated the AnsiColors class

--- AnsiColors.java
+++ AnsiColors.java
@@ -4,28 +4,11 @@
  * After done with those colors, be sure to reset!
 */
 public class AnsiColors {
-    final String ANSI_RESET = "\u001B[0m";
-    final String ANSI_BLACK = "\u001B[30m";
-    final String ANSI_RED = "\u001B[31m";
-    final String ANSI_GREEN = "\u001B[32m";
-    final String ANSI_YELLOW = "\u001B[33m";
-    final String ANSI_BLUE = "\u001B[34m";
-    final String ANSI_PURPLE = "\u001B[35m";
-    final String ANSI_CYAN = "\u001B[36m";
-    final String ANSI_WHITE = "\u001B[37m";
-    final String ANSI_BLACK_BACKGROUND = "\u001B[40m";
-    final String ANSI_RED_BACKGROUND = "\u001B[41m";
-    final String ANSI_GREEN_BACKGROUND = "\u001B[42m";
-    final String ANSI_YELLOW_BACKGROUND = "\u001B[43m";
-    final String ANSI_BLUE_BACKGROUND = "\u001B[44m";
-    final String ANSI_PURPLE_BACKGROUND = "\u001B[45m";
-    final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
-    final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
-
     /**
      * Resets the color of the text.
     */
     public String reset() {
+        final String ANSI_RESET = "\u001B[0m";
         return ANSI_RESET;
     }
 
@@ -33,48 +16,56 @@ public class AnsiColors {
      * Changes the color of the text to black.
     */
     public String black() {
+        final String ANSI_BLACK = "\u001B[30m";
         return ANSI_BLACK;
     }
     /**
      * Changes the color of the text to red.
     */
     public String red() {
+        final String ANSI_RED = "\u001B[31m";
         return ANSI_RED;
     }
     /**
      * Changes the color of the text to green.
     */
     public String green() {
+        final String ANSI_GREEN = "\u001B[32m";
         return ANSI_GREEN;
     }
     /**
      * Changes the color of the text to yellow.
     */
     public String yellow() {
+        final String ANSI_YELLOW = "\u001B[33m";
         return ANSI_YELLOW;
     }
     /**
      * Changes the color of the text to blue.
     */
     public String blue() {
+        final String ANSI_BLUE = "\u001B[34m";
         return ANSI_BLUE;
     }
     /**
      * Changes the color of the text to purple.
     */
     public String purple() {
+        final String ANSI_PURPLE = "\u001B[35m";
         return ANSI_PURPLE;
     }
     /**
      * Changes the color of the text to cyan.
     */
     public String cyan() {
+        final String ANSI_CYAN = "\u001B[36m";
         return ANSI_CYAN;
     }
     /**
      * Changes the color of the text to white.
     */
     public String white() {
+        final String ANSI_WHITE = "\u001B[37m";
         return ANSI_WHITE;
     }
 
@@ -82,48 +73,56 @@ public class AnsiColors {
      * Changes the color of the background of the text to black.
     */
     public String blackBackground() {
+        final String ANSI_BLACK_BACKGROUND = "\u001B[40m";
         return ANSI_BLACK_BACKGROUND;
     }
     /**
      * Changes the color of the background of the text to red.
     */
     public String redBackground() {
+        final String ANSI_RED_BACKGROUND = "\u001B[41m";
         return ANSI_RED_BACKGROUND;
     }
     /**
      * Changes the color of the background of the text to green.
     */
     public String greenBackground() {
+        final String ANSI_GREEN_BACKGROUND = "\u001B[42m";
         return ANSI_GREEN_BACKGROUND;
     }
     /**
      * Changes the color of the background of the text to yellow.
     */
     public String yellowBackground() {
+        final String ANSI_YELLOW_BACKGROUND = "\u001B[43m";
         return ANSI_YELLOW_BACKGROUND;
     }
     /**
      * Changes the color of the background of the text to blue.
     */
     public String blueBackground() {
+        final String ANSI_BLUE_BACKGROUND = "\u001B[44m";
         return ANSI_BLUE_BACKGROUND;
     }
     /**
      * Changes the color of the background of the text to purple.
     */
     public String purpleBackground() {
+        final String ANSI_PURPLE_BACKGROUND = "\u001B[45m";
         return ANSI_PURPLE_BACKGROUND;
     }
     /**
      * Changes the color of the background of the text to cyan.
     */
     public String cyanBackground() {
+        final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
         return ANSI_CYAN_BACKGROUND;
     }
     /**
      * Changes the color of the background of the text to white.
     */
     public String whiteBackground() {
+        final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
         return ANSI_WHITE_BACKGROUND;
     }
 }
